6713f883c29e33864045eea808580f8b501f1943
xhc16340263 <31815522+xhc16340263@users.noreply.github.com>
2018-04-24 23:25:10 +0800

    Add files via upload

---
+++ Assets/Controller.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+	public static int score=0;
+	public void OnGUI(){
+		GUI.Button (new Rect (400, 20, 80, 30), "分数" + score);
+	}
+	// Use this for initialization
+	void Start () {
+		disk = DiskFactory.getDisk ();
+		if (disk.onClick ()) {
+			addScore ();
+			free (disk);
+		}
+	}
+	void addScore(){
+		score++;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
